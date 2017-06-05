e3b92d33d7c6bbb8f1a14586df07763ccf52b5a9
Julio Brazil <juliobbl@Julios-MacBook-Pro.local>
2017-06-05 18:11:00 -0300

    Já tinha um problema, arrumei

---
+++ MiniChallenge03/FirebaseProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseProtocol.swift
+//  MiniChallenge03
+//
+//  Created by Julio Brazil on 05/06/17.
+//  Copyright © 2017 Julio Brazil LTDA. All rights reserved.
+//
+
+import Foundation
+
+public protocol FirebaseProtocol {
+    
+    var instance: FirebaseProtocol { get }
+    
+    
+    
+}
