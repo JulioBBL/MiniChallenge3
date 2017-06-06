efce10fc68b05c6a15cfbe9849d7e47a26abc995
Julio Brazil <juliobbl@Julios-MacBook-Pro.local>
2017-06-06 16:59:55 -0300

    tela de informações iniciada

---
+++ MiniChallenge03/BankCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BankCollectionViewCell.swift
+//  MiniChallenge03
+//
+//  Created by Julio Brazil on 06/06/17.
+//  Copyright © 2017 Julio Brazil LTDA. All rights reserved.
+//
+
+import UIKit
+
+class BankCollectionViewCell: UICollectionViewCell {
+    
+}
