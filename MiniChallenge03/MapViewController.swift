4fefa6f59efbaffcff21cbd5389aa0b701d6f00e
Ricardo <ricardodnsousa@gmail.com>
2017-06-09 15:52:19 -0300

    hemocentros updates

--- MiniChallenge03/MapViewController.swift
+++ MiniChallenge03/MapViewController.swift
@@ -11,38 +11,44 @@ import MapKit
 import CoreLocation
 import Firebase
 
-//class MapViewController: ViewController, MKMapViewDelegate, CLLocationManagerDelegate, FIRDatabaseReferenceable {
+class MapViewController: ViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-//    var hemocentros: [Hemocentro] = []
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        locationManager.requestWhenInUseAuthorization()
-//        map.showsUserLocation = true
-//        locationManager.delegate = self
-//        
-//        ref.child("hemocetros").observe(.childAdded, with: {
-//            self.hemocentros.append(Hemocentro(snapshot: $0, subTitulo: ""))
-//        })
-//    }
-//    
-//    
-//    
-//    @IBOutlet var map: MKMapView!
-//    
-//    var locationManager = CLLocationManager()
-//    
-//    func addPin(title: String, subTitle: String, location: CLLocation){
-//        let position :CLLocation = location
-//        
-//        let ann = MKPointAnnotation()
-//        ann.coordinate = position.coordinate
-//        ann.title = title
-//        ann.subtitle = subTitle
-//        
-//        map.addAnnotation(ann)
-//    }
+    var hemocentros: [Hemocentro] = []
+    
+    var ref = FIRDatabase.database().reference(withPath: "hemocentros")
     
     
     
-//}
+    
+    @IBOutlet var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        map.showsUserLocation = true
+        locationManager.delegate = self
+        
+        ref.child("hemocetros").observe(.childAdded, with: {snapshot in
+            self.hemocentros.append(Hemocentro(snapshot: snapshot))
+        })
+        
+        
+    }
+
+    
+    func addPin(title: String, subTitle: String, location: CLLocation){
+        let position :CLLocation = location
+        
+        let ann = MKPointAnnotation()
+        ann.coordinate = position.coordinate
+        ann.title = title
+        ann.subtitle = subTitle
+        
+        map.addAnnotation(ann)
+    }
+    
+    
+    
+}
