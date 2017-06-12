7298cb9344bf979bf9ebe9b9c1bf8574267ebbcd
Ricardo <ricardodnsousa@gmail.com>
2017-06-12 17:37:18 -0300

    FirebaseConnection Class

---
+++ MiniChallenge03/FirebaseConnection.swift
@@ -0,0 +1,41 @@
+//
+//  FirebaseConnection.swift
+//  MiniChallenge03
+//
+//  Created by Ricardo Sousa on 12/06/17.
+//  Copyright © 2017 Julio Brazil LTDA. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseConnection {
+    static let sharedInstance = FirebaseConnection()
+    
+    
+    var ref = FIRDatabase.database().reference()
+    
+    static var hemocentros: [Hemocentro] = []
+    static var usuarioAtual: User?
+//    var doacoes: [Donation] = []
+//    var usuarios: [User] = []
+    
+    
+    
+    private init(){
+        ref.child("hemocetros").observe(.childAdded, with: {snapshot in
+            FirebaseConnection.hemocentros.append(Hemocentro(snapshot: snapshot))
+        })
+        
+        FIRAuth.auth()?.addStateDidChangeListener { auth, user in
+            if user != nil {
+            
+            } else {
+                // No user is signed in.
+            }
+        }
+    }
+    
+    
+    
+}
