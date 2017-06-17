35716d2845b3f742c65281ad90d9d315149ccd9a
Julio Brazil <julio_bbl@hotmail.com>
2017-06-17 17:10:08 -0300

    tela informação finalizada

--- MiniChallenge03/InfoCollectionViewController.swift
+++ MiniChallenge03/InfoCollectionViewController.swift
@@ -20,22 +20,23 @@ class InfoCollectionViewController: UICollectionViewController, UICollectionView
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier?.contains("toDonatesTo"))! {
+            (segue.destination as! WhoDonatesToViewController).donor = BloodType(rawValue: ((sender as! UIButton).titleLabel?.text)!)!
+        }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
     
     
     // MARK: - Collection View Delegate
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 4
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -43,13 +44,11 @@ class InfoCollectionViewController: UICollectionViewController, UICollectionView
         
         switch indexPath.row {
         case 0:
-            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "estoque", for: indexPath)
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "doaRecebe", for: indexPath)
         //            cell.frame.size.height = 100
         case 1:
-            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "doaRecebe", for: indexPath)
-        case 2:
             cell = collectionView.dequeueReusableCell(withReuseIdentifier: "processoDoacao", for: indexPath)
-        case 3:
+        case 2:
             cell = collectionView.dequeueReusableCell(withReuseIdentifier: "teste", for: indexPath)
         default:
             cell = collectionView.dequeueReusableCell(withReuseIdentifier: "outros", for: indexPath)
@@ -66,10 +65,8 @@ class InfoCollectionViewController: UICollectionViewController, UICollectionView
         case 0:
             return CGSize(width: largura - 16, height: 200)
         case 1:
-            return CGSize(width: largura - 16, height: 50)
-        case 2:
             return CGSize(width: largura - 16, height: 250)
-        case 3:
+        case 2:
             return CGSize(width: largura - 16, height: 108)
         default:
             return CGSize(width: largura - 16, height: 668)
---
+++ WhoDonatesToViewController.swift
@@ -0,0 +1,158 @@
+//
+//  WhoDonatesToViewController.swift
+//  MiniChallenge03
+//
+//  Created by Julio Brazil on 17/06/17.
+//  Copyright © 2017 Julio Brazil LTDA. All rights reserved.
+//
+
+import UIKit
+
+class WhoDonatesToViewController: UIViewController {
+    
+    var donor: BloodType = .oNegative
+    
+    @IBOutlet weak var donorsLabel: UILabel!
+    
+    @IBOutlet weak var oPReceives: UILabel!
+    @IBOutlet weak var oNReceives: UILabel!
+    @IBOutlet weak var aPReceives: UILabel!
+    @IBOutlet weak var aNReceives: UILabel!
+    @IBOutlet weak var bPReceives: UILabel!
+    @IBOutlet weak var bNReceives: UILabel!
+    @IBOutlet weak var abPReceives: UILabel!
+    @IBOutlet weak var abNReceives: UILabel!
+
+    @IBOutlet weak var oPDonates: UILabel!
+    @IBOutlet weak var oNDonates: UILabel!
+    @IBOutlet weak var aPDonates: UILabel!
+    @IBOutlet weak var aNDonates: UILabel!
+    @IBOutlet weak var bPDonates: UILabel!
+    @IBOutlet weak var bNDonates: UILabel!
+    @IBOutlet weak var abPDonates: UILabel!
+    @IBOutlet weak var abNDonates: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.tabBar.isHidden = true
+
+        self.donorsLabel.text = self.donor.rawValue
+        
+        switch self.donor {
+        case .oNegative:
+            self.oPReceives.alpha = 0.3
+            self.aPReceives.alpha = 0.3
+            self.aNReceives.alpha = 0.3
+            self.bPReceives.alpha = 0.3
+            self.bNReceives.alpha = 0.3
+            self.abPReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+        case .oPositive:
+            self.aPReceives.alpha = 0.3
+            self.aNReceives.alpha = 0.3
+            self.bPReceives.alpha = 0.3
+            self.bNReceives.alpha = 0.3
+            self.abPReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+            self.oNDonates.alpha = 0.3
+            self.aNDonates.alpha = 0.3
+            self.bNDonates.alpha = 0.3
+            self.abNDonates.alpha = 0.3
+            
+        case .aNegative:
+            self.oPReceives.alpha = 0.3
+            self.aPReceives.alpha = 0.3
+            self.bPReceives.alpha = 0.3
+            self.bNReceives.alpha = 0.3
+            self.abPReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+            self.oNDonates.alpha = 0.3
+            self.oPDonates.alpha = 0.3
+            self.bNDonates.alpha = 0.3
+            self.bPDonates.alpha = 0.3
+            
+        case .aPositive:
+            self.bPReceives.alpha = 0.3
+            self.bNReceives.alpha = 0.3
+            self.abPReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+            self.oNDonates.alpha = 0.3
+            self.oPDonates.alpha = 0.3
+            self.aNDonates.alpha = 0.3
+            self.bNDonates.alpha = 0.3
+            self.bPDonates.alpha = 0.3
+            self.abNDonates.alpha = 0.3
+            
+        case .bNegative:
+            self.oPReceives.alpha = 0.3
+            self.aNReceives.alpha = 0.3
+            self.aPReceives.alpha = 0.3
+            self.bPReceives.alpha = 0.3
+            self.abPReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+            self.oNDonates.alpha = 0.3
+            self.oPDonates.alpha = 0.3
+            self.aNDonates.alpha = 0.3
+            self.aPDonates.alpha = 0.3
+            
+        case .bPositive:
+            self.aNReceives.alpha = 0.3
+            self.aPReceives.alpha = 0.3
+            self.abPReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+            self.oNDonates.alpha = 0.3
+            self.oPDonates.alpha = 0.3
+            self.aNDonates.alpha = 0.3
+            self.aPDonates.alpha = 0.3
+            self.bNDonates.alpha = 0.3
+            self.abNDonates.alpha = 0.3
+            
+        case .abNegative:
+            self.oNReceives.alpha = 0.3
+            self.aNReceives.alpha = 0.3
+            self.bNReceives.alpha = 0.3
+            self.abNReceives.alpha = 0.3
+            
+            self.oNDonates.alpha = 0.3
+            self.oPDonates.alpha = 0.3
+            self.aPDonates.alpha = 0.3
+            self.aNDonates.alpha = 0.3
+            self.bPDonates.alpha = 0.3
+            self.bNDonates.alpha = 0.3
+            
+        default:
+            self.oNDonates.alpha = 0.3
+            self.oPDonates.alpha = 0.3
+            self.aNDonates.alpha = 0.3
+            self.aPDonates.alpha = 0.3
+            self.bNDonates.alpha = 0.3
+            self.bPDonates.alpha = 0.3
+            self.abNDonates.alpha = 0.3
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
