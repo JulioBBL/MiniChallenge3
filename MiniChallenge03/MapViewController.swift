9c558fdaa5bee41bfdbb445a2454a2719056529c
Ricardo <ricardodnsousa@gmail.com>
2017-06-18 22:07:42 -0300

    Auto stash before merge of "master" and "origin/master"

--- MiniChallenge03/MapViewController.swift
+++ MiniChallenge03/MapViewController.swift
@@ -20,6 +20,7 @@ class MapViewController: ViewController, MKMapViewDelegate, CLLocationManagerDel
     
     
     
+    
     @IBOutlet var map: MKMapView!
     
     var locationManager = CLLocationManager()
@@ -29,6 +30,8 @@ class MapViewController: ViewController, MKMapViewDelegate, CLLocationManagerDel
         locationManager.requestWhenInUseAuthorization()
         map.showsUserLocation = true
         locationManager.delegate = self
+        map.delegate = self
+        
         
         //        if let location = map.userLocation.location {
         //            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 200 * 2.0, 200 * 2.0)
@@ -40,44 +43,51 @@ class MapViewController: ViewController, MKMapViewDelegate, CLLocationManagerDel
         
         //Zoom to user location
         if let noLocation = locationManager.location?.coordinate {
-        let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 500, 500)
-        map.setRegion(viewRegion, animated: false)
+            let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 500, 500)
+            map.setRegion(viewRegion, animated: false)
         }
         
         FirebaseConnection.getHemocentros {snapshot in
             for hemo in snapshot.children{
                 self.addPin(hemocentro: Hemocentro(snapshot: hemo as! FIRDataSnapshot))
-             
+                
             }
         }
+        
         
     }
     
     
     func addPin(hemocentro: Hemocentro){
         let position :CLLocation = CLLocation(latitude: CLLocationDegrees(hemocentro.latitude)!, longitude: CLLocationDegrees(hemocentro.longitude)!)
-        let ann = MKPointAnnotation()
+        
+        let ann = HemoPin()
+        
+        ann.pinCustomImageName = "hemopin"
         ann.coordinate = position.coordinate
         ann.title = hemocentro.nome
         ann.subtitle = hemocentro.endereco
         
-        map.addAnnotation(ann)
+        let pinAnnotationView = MKAnnotationView(annotation: ann, reuseIdentifier: "pin")
+        map.addAnnotation(pinAnnotationView.annotation!)
     }
     
-    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 50 * 2.0, 50 * 2.0)
-        map.setRegion(coordinateRegion, animated: false)
-        
-    }
-    
-    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
-        if let location = mapView.userLocation.location {
-            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 200 * 2.0, 200 * 2.0)
-            map.setRegion(coordinateRegion, animated: true)
-        }else{
-            print("deu erro")
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? HemoPin{
+            
+            let view = MKAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            //TROCANDO IMAGEM DO PIN
+            view.image = #imageLiteral(resourceName: "hemopin")
+//            view.centerOffset = CGPoint(x: 0, y: -view.bounds.height/2)
+            view.isEnabled = true
+            view.canShowCallout = true
+            
+            return view
         }
+        
+        return nil
     }
+    
+    
     
 }
